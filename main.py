a75ee2b8fae4e3bb0e713c265fcb60a13f6c4b79
samg111 <berrystump@protonmail.com>
2026-03-26 14:02:13 -0400

    refactor: Enhance main program flow with error handling and cleanup for drone operations

--- main.py
+++ main.py
@@ -1,34 +1,50 @@
+import os
+
+# Suppress harmless OpenCV/Qt warnings
+os.environ["QT_QPA_PLATFORM"] = "xcb"
+os.environ["QT_LOGGING_RULES"] = "*.debug=false;*.info=false;*.warning=false" 
+
 import time
 import cv2
-from djitellopy import Tello
-import face_recognition
 
 from drone_controller import DroneController
 from face_recognizer import FaceRecognizer
 
 def main():  
     time.sleep(5)
+    whileCount = 0
+    drone = DroneController()
     face_recognizer = FaceRecognizer("ref_img.jpg")
 
-    drone = DroneController()
-    drone.connect_and_setup()
-    drone.takeoff()
-    whileCount = 0
+    try:
+        drone.connect_and_setup()
+        drone.takeoff()
 
-    while True:
-        whileCount += 1
-        if whileCount % 100 == 0:
-            print("whileCount: " + str(whileCount))
-        if drone.check_for_quit():
-            break
-        img = drone.get_frame()
-        face_locations, face_matches = face_recognizer.recognize_faces(img)
-        img = face_recognizer.draw_faces(img, face_locations, face_matches)
-        cv2.imshow("Drone Camera", img)
-        
+        while True:
+            whileCount += 1
+            if whileCount % 10 == 0:
+                print("whileCount: " + str(whileCount))
+            if drone.check_for_quit():
+                break
+            img = drone.get_frame()
+            face_locations, face_matches = face_recognizer.recognize_faces(img)
+            img = face_recognizer.draw_faces(img, face_locations, face_matches)       
 
-    print("exited while loop")
-    drone.land()
+            cv2.imshow("Drone Camera", img)
+
+            time.sleep(0.03)
+
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+
+    except KeyboardInterrupt:
+        print("Keyboard interrupt detected (ctrl+c). shutting down...")
+
+    finally:
+        print("Cleaning up and landing drone...")
+        drone.land()
+        cv2.destroyAllWindows()
+        print("Drone landed and windows closed")
 
 if __name__ == "__main__":
     main()
